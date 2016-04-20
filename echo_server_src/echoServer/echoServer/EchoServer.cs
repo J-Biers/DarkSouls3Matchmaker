698bfc1d34f0521b6dd4db553fb685b7192f29f4
Alex Shelley <ashelley@purdue.edu>
2016-04-20 13:08:05 -0400

    initial commit

---
+++ echo_server_src/echoServer/echoServer/EchoServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+
+namespace echoServer
+{
+    public class EchoServer
+    {
+        public HttpListener listener = new HttpListener();
+
+        public static void Main(string[] args)
+        {
+            EchoServer server = new EchoServer();
+        }
+
+        public void Start()
+        {
+            //TODO: Start listening and echoing
+        }
+    }
+}
