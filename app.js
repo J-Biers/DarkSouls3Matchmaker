7a3f22df0c03d52d871fcd6e46db59eeb8cbeb11
Alex Shelley <ashelley@purdue.edu>
2016-04-22 02:22:36 +0000

    commit messages are useless at this point

--- app.js
+++ app.js
@@ -22,8 +22,26 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
+/*
 app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+*/
+
+//My crack at making the server
+var http = require('http');
+
+var server = http.createServer();
+server.listen();
+
+//onRequest event
+server.on('request', function(request, response)
+{
+	console.log("server received request.");
+	console.log("port is " + appEnv.port);
+	
+	response.write("Hello world!");
+	response.end();
 });
